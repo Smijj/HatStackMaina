85c48bf396a4d2108e3999a4c92a04f758bf9055
Declan Smyth <territoryaisolutions@gmail.com>
2024-10-24 12:01:00 +0930

    Finished core functionality

--- gd.hatstackmania/Scripts/HatCtrl.gd
+++ gd.hatstackmania/Scripts/HatCtrl.gd
@@ -7,34 +7,63 @@ enum HatStates {
 	Placed
 }
 
-@onready var m_HatSprite: Sprite2D = $Sprite2D
+signal HatDone(wasSuccessful:bool)
 
+@export_group("Settings")
+@export var m_TravelTime: float = 1
+@export var m_TravelTimeCounter: float = 0
 @export var m_MoveSpeed: float = 400
-@export var m_PossibleHatSprites: Array[Texture]
+
+@export_group("Refs")
+@export var m_HatSprite: Sprite2D
 
 var m_HatState: HatStates = HatStates.None
-var m_TargetPos: Vector2
-var m_EndPos: Vector2
+var m_TargetPos: Vector2 = Vector2.ZERO
+var m_StartPos: Vector2 = Vector2.ZERO
+var m_EndPos: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	# Move Hat based on state
 	if m_HatState == HatStates.Moving:
-		global_position = global_position.move_toward(m_EndPos, m_MoveSpeed * delta)
-	
-	pass
+		global_position = lerp(m_StartPos, m_EndPos, m_TravelTimeCounter/m_TravelTime)
+		
+		if m_TravelTimeCounter < m_TravelTime:
+			m_TravelTimeCounter += delta
+		else: 
+			Destroy()
 
-func Init(target: Vector2, endPos: Vector2) -> void:
+		#global_position = global_position.move_toward(m_EndPos, m_MoveSpeed * delta)
+
+func Init(hatSprite: Texture, target: Vector2, startPos: Vector2, endPos: Vector2) -> Signal:
+	m_HatSprite.texture = hatSprite
+	m_TargetPos = target
+	m_StartPos = startPos
+	m_EndPos = endPos
+	
 	m_HatState = HatStates.Moving
 	
-	pass
+	return HatDone
 
-func Stop() -> void:
-	m_HatState= HatStates.Placed
+func Stop(errorMargin: float = 0) -> bool:
+	if IsSuccessful(errorMargin):
+		m_HatState = HatStates.Placed
+		HatDone.emit(true)
+		return true
 	
-	pass
+	# Destroy Hat
+	Destroy()
+	return false
+
+func IsSuccessful(errorMargin: float = 0) -> bool:
+	if _GetDistanceFromTarget() > errorMargin: 
+		return false
+	return true
+
+func _GetDistanceFromTarget() -> float:
+	var distance: float = global_position.distance_to(m_TargetPos)
+	print('Distance to Target' + str(distance))
+	return distance
+
+func Destroy() -> void:
+	HatDone.emit(false)
+	queue_free()
--- gd.hatstackmania/Scripts/HatSpawnCtrl.gd
+++ gd.hatstackmania/Scripts/HatSpawnCtrl.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
-@export var m_HatMoveSpeed: float = 100
+@export_group("Settings")
+@export var m_InputMarginOfErrorInPixels: float = 100
+@export var m_SuccessfulHatMoveDownIncrement: float = 150
 @export var m_HatPrefab: PackedScene
+@export var m_PossibleHatSprites: Array[Texture]
 
+@export_group("Refs")
+@export var m_LilMan: Node2D
 @export var m_HatParent: Node2D
 @export var m_SpawnPosLeft: Node2D
 @export var m_SpawnPosRight: Node2D
-@export var m_HitPosIndicator: Node2D
+@export var m_HitPosIndicator: Sprite2D
 
 
 var m_PlacedHats: Array[HatCtrl] = []
@@ -14,32 +19,48 @@ var m_ActiveHat: HatCtrl = HatCtrl.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	# Spawn Hat on either the left of the right
-	m_ActiveHat = m_HatPrefab.instantiate()
-	m_HatParent.add_child(m_ActiveHat)
-	m_ActiveHat.global_position = m_SpawnPosLeft.global_position
-	
-	print("Spawned Hat")
-	
+	SpawnHat()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	
-	
-	# Move hat towards opposite side that it spawnned
-	m_ActiveHat.global_position = m_ActiveHat.global_position.move_toward(m_SpawnPosRight.global_position, delta * m_HatMoveSpeed)
-	#m_ActiveHat.global_position.lerp(m_SpawnPosRight.global_position, 0.5)
-	print("Moving Hat" + str(m_ActiveHat.global_position))
-	
+func _input(event: InputEvent) -> void:
 	# Check if the player inputs to stop the hat at the correct time (or within a margin of error)
 	# If they do; stop the hat from moving, add it to the PlacedHats array, and move it to a new Parent node for PlacedHats
 	# If they dont; Fade the hat out then deinstantiate it
+	if event.is_action_pressed("MainInput"):
+		m_ActiveHat.Stop(m_InputMarginOfErrorInPixels)
+		
+
+func SpawnHat() -> void:
+	# Select Random Hat Sprite
+	var hatSprite: Texture = m_PossibleHatSprites.pick_random()
+	
+	# Set the Hat Indicator texture
+	m_HitPosIndicator.texture = hatSprite
+	
+	# Spawn Hat on either the left of the right
+	var spawnPos: Vector2
+	var endPos: Vector2
+	if randi_range(0, 1) == 0:
+		spawnPos = m_SpawnPosLeft.global_position 
+		endPos = m_SpawnPosRight.global_position 
+	else:
+		spawnPos = m_SpawnPosRight.global_position 
+		endPos = m_SpawnPosLeft.global_position
+	
+	# Instantiate new Hat Object and make it a child of the HatParent Node
+	m_ActiveHat = m_HatPrefab.instantiate()
+	m_HatParent.add_child(m_ActiveHat)
+	m_ActiveHat.global_position = spawnPos
+	m_ActiveHat.Init(hatSprite, m_HitPosIndicator.global_position, spawnPos, endPos).connect(OnActiveHatDone)
+
+func OnActiveHatDone(wasSuccessful: bool) -> void:
+	if wasSuccessful:
+		m_PlacedHats.append(m_ActiveHat)
+		if m_LilMan: m_LilMan.global_position.y += m_SuccessfulHatMoveDownIncrement
+		print("Was Successful")
+	else:
+		print("Was Not Successful")
+		pass
 	
 	# Once the hat has been placed or deleted, Start process again
-	
-	
-	
-	pass
+	m_ActiveHat = null
+	SpawnHat()
